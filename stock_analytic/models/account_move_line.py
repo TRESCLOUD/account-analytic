4192c99b6fe20a01c35b584708ef8d2d072810f9
Steven Luna <steven.luna@trescloud.com>
2024-09-05 11:18:17 -0500

    [IMP] stock_analytic: Mejora a comentario

--- stock_analytic/models/account_move_line.py
+++ stock_analytic/models/account_move_line.py
@@ -7,8 +7,7 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'    
 
     def _prepare_analytic_distribution_line(self, distribution, account_id, distribution_on_each_plan):
-        # FIX TRESCLOUD: Arreglo en la creación de apuntes analíticos para que la cantidad sea positiva
-        # Los apuntes analíticos siempre deben ser positivas y en el caso de que se creen a partir de un movimiento de salida o de desecho, la cantidad debe ser positiva
+        # Los apuntes analíticos siempre deben ser positivas y más aún casos en que se creen a partir de un movimiento de salida o de desecho, donde su cantidad es negativa
         vals = super()._prepare_analytic_distribution_line(distribution, account_id, distribution_on_each_plan)
 
         if self.quantity < 0:
